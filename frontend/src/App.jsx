a37b7463dadd3d7ade19f2619d6ccd58e9c029d2
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-03-03 18:58:15 +0530

    added the images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/navbar/navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+const navbar = () => {
+  return (
+    <div className='navbar'>
+        
+    </div>
+  )
+}
+
+export default navbar
