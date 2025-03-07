5114d44937cbf53434961c7dcc7100b8911266fd
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-03-07 10:27:33 +0530

    completed home section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import {BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import Home from './pages/home/home'
+import Cart from './pages/cart/Cart'
+import Order from './pages/placeorder/Order'
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeorder' element={<Order/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Craving something delicious? Order food from your favorite restaurants and have it delivered straight to your doorstep. Whether you're in the mood for a hearty meal, a quick snack, or a sweet treat, we've got you covered. Enjoy the convenience of hassle-free ordering, fast delivery. Satisfy your cravings with just a few taps and let us bring your favorite flavors to you!</p>
+            <button>view menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
