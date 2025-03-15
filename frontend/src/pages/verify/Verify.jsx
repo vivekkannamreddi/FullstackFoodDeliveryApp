1865320d2d6440ea5b03e20b1d7ce8deac2950a1
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-03-15 19:45:10 +0530

    deleted and re-created the verify files

--- frontend/src/pages/verify/Verify.jsx
+++ frontend/src/pages/verify/Verify.jsx
@@ -1,38 +1,39 @@
-import React, { useContext, useEffect } from 'react'
-import './verify.css'
+import React from 'react'
+import './Verify.css'
+import  { useContext, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
-const verify = () => {
 
+const Verify = () => {
     const [searchParams,setSearchParams] = useSearchParams();
-    const success = searchParams.get("success");
-    const orderId = searchParams.get("orderId");
-    const navigate = useNavigate();
+        const success = searchParams.get("success");
+        const orderId = searchParams.get("orderId");
+        const navigate = useNavigate();
+        
+        const {url} = useContext(StoreContext);
     
-    const {url} = useContext(StoreContext);
-
-    const verifypayment = async()=>{
-        const response = await axios.post(url+"/api/order/verify",{success,orderId});
-        if(response.data.success){
-            navigate("/myorders")
+        const verifypayment = async()=>{
+            const response = await axios.post(url+"/api/order/verify",{success,orderId});
+            if(response.data.success){
+                navigate("/myorders")
+            }
+            else{
+                navigate("/")
+            }
         }
-        else{
-            navigate("/")
-        }
+        useEffect(()=>{
+            verifypayment();
+        },[])
+    
+    
+      return (
+        <div className='verify'>
+            <div className="spinner">
+                
+            </div>        
+        </div>
+      )
     }
-    useEffect(()=>{
-        verifypayment();
-    },[])
 
-
-  return (
-    <div className='verify'>
-        <div className="spinner">
-            
-        </div>        
-    </div>
-  )
-}
-
-export default verify
+export default Verify
